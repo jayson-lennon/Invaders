58b2160405271042c15e92c9080ae4712daf4b4e
Jayson Lennon <jay.dub.lennon@gmail.com>
2016-12-30 02:40:36 -0800

    Update main() for image create removal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 extern crate invaderlib;
+use invaderlib::{ImageTool, Grid};
+use invaderlib::Gen::Generator;
+use ImageTool::{new_file, save_grid, GridExporter};
 
 fn main() {
-    println!("Hello, world!");
+    let mut gen = Generator::new();
+    let invader = gen.invader(16, 16, 6, 10);
+    if let Ok(fh) = new_file("invader.png") {
+        if save_grid(fh, &invader, 16, 16).is_err() {
+            println!("Failed composing image data");
+        }
+    }
 }

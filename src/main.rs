c3c10670f2d8e71a4f170a5ea0af737bf838c3bc
Jayson Lennon <jay.dub.lennon@gmail.com>
2016-12-30 07:50:16 -0800

    Update main() invader sig

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use ImageTool::{new_file, save_grid, GridExporter};
 
 fn main() {
     let mut gen = Generator::new();
-    let invader = gen.invader(16, 16, 6, 10);
+    let invader = gen.invader(8, 16, (7, 7), (8, 14), (1, 1), (1, 5));
     if let Ok(fh) = new_file("invader.png") {
         if save_grid(fh, &invader, 16, 16).is_err() {
             println!("Failed composing image data");

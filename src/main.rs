3c3c81e71e04e8969b43e466f9b93e3c6e96d6c9
Jayson Lennon <jay.dub.lennon@gmail.com>
2017-01-01 15:21:19 -0800

    Add params in main() for invader

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,39 @@ use ImageTool::{new_file, save_grid, GridExporter};
 
 fn main() {
     let mut gen = Generator::new();
-    loop {
-        if let Some(invader) = gen.invader(8, 8, (7, 7), (8, 14), (1, 3), (1, 2), 0) {
-            if let Ok(fh) = new_file("invader.png") {
-                if save_grid(fh, &invader, 16, 16).is_err() {
-                    println!("Failed composing image data");
-                }
-                break;
+    let mut num_generated = 0;
+    let mut sheet = Grid::new();
+    let mut rows = 0;
+    let num_per_row = 8;
+    let mut failed_gens = 0;
+    while num_generated < 64 {
+        if failed_gens >= 1000 {
+            println!("Maximum number of failed generations reached. Stopping generation.");
+            break;
+        }
+        if let Some(invader) = gen.invader(8, 8, 5, 12, (1, 3), (1, 5), 1) {
+            let width = invader.size().0;
+            sheet.merge_at(&invader,
+                           (num_generated % num_per_row) * 8 * 2 + (16 / width),
+                           rows * 8 * 2);
+            if (num_generated + 1) % num_per_row == 0 && num_generated > 0 {
+                rows += 1;
             }
+        } else {
+            failed_gens += 1;
+            println!("Failed to generate an invader. Retry #{}", failed_gens);
+            continue;
+        }
+        num_generated += 1;
+    }
+    if let Ok(fh) = new_file("invader.png") {
+        if save_grid(fh,
+                     &sheet,
+                     (num_generated / num_per_row * 8 * 2) as u32,
+                     (num_generated / num_per_row * 8 * 2) as u32)
+            .is_err() {
+            println!("Failed composing image data");
         }
     }
+
 }
